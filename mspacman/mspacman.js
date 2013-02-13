ba574949112105c8174c6eca252587feef1de1d2
Caroline Marcks <caroline.marcks@tufts.edu>
2013-02-13 17:40:32 -0500

    drawmspacman on load

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -1,15 +1,23 @@
 function drawmspacman() {
+			img=document.spritesheet;
+			h=window.innerHeight;
+			w=window.innerWidth;
+
+			x=(w-465)/2;
+			y=(h-140)/2
+
             canvas = document.getElementById('mspacman');
+            console.log(spritesheet.complete);
             if (canvas.getContext) {
                 ctx = canvas.getContext('2d');
-                ctx.clearRect(0, 0, 600, 600); // clear the screen
-            	img =  new Image();
-            	img.src="pacman10-hp-sprite.png"
-            	ctx.drawImage(img,320,0,465,140,0,0,465,140);
-            	ctx.drawImage(img,0,0,20,20,100,93,19,19);
-            	ctx.drawImage(img,0,99,20,20,133,93,19,19);	
+                ctx.clearRect(0, 0, 1000, 1000); // clear the screen
+            	ctx.drawImage(img,320,0,465,140,x,y,465,140);
+            	ctx.drawImage(img,0,0,20,20,x+100,y+93,19,19);
+            	ctx.drawImage(img,0,99,20,20,x+133,y+93,19,19);	
             }
             else {
                 alert('Sorry, canvas is not supported on your browser!');
             }
+          
 }
+window.addEventListener('load',drawmspacman,false);
