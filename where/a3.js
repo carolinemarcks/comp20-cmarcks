0f68c311d2b6abee4ccd596241c0385ea1d22706
Caroline Marcks <caroline.marcks@tufts.edu>
2013-03-04 17:27:12 -0500

    calc and display distance

--- where/a3.js
+++ where/a3.js
@@ -1,32 +1,21 @@
 var map;
-/*
-var redStations = [];
-var pkeyToStationName = [];
-var keyData=[];
-var stationOrder=[];
-*/
-
-  /*infowindow.setContent("I HAPPEN???");*/
-var todisplay;
 var myMarker;
-var people;
-var redLineMarker;
+var myPos;
+var redLineMarkers;
 var dRequest;
 var cwRequest;
 var arrivalData;
 var geoLoaded;
-var stationsLoaded;
 var waldo;
 var carmen;
 
 function initialize(){
-  // Check if geolocation is supported by browser
   if(!navigator.geolocation) {
     throw 'Geolocation not supported!';
   }else {
     initGlobals();
-	  //initialize map to cambridge area
-    var latlng = new google.maps.LatLng(42.37,-71.13);
+    //initialize map to cambridge area
+    var latlng = new google.maps.LatLng(42.37,-71.08);
     myOptions = {
       zoom: 12,
       center: latlng,
@@ -40,31 +29,71 @@ function initialize(){
 
     //set up redline T stops
     placeRedLineStations();
-    getCarmenAndWaldo();
+    //retrieve and place Carmen and Waldo
+    setCarmenAndWaldo();
+    //retrieve and set train info
     setArrivalData();
   }
 }
 
+
 function initGlobals(){
-  people=new Object;
-  redLineMarker = new Object();
+  redLineMarkers = new Object();
   geoLoaded=false;
-  stationsLoaded=false;
   waldo=false;
   carmen=false;
 }
+
+function toRad(x){
+    return x * Math.PI / 180;
+}
+  
+function hdistance(lat1,lon1){
+  return function(lat2,lon2){
+    var R = 3959;
+
+    var x1 = lat1-lat2;
+    var dLat = toRad(x1);  
+    var x2 = lon1-lon2; 
+    var dLon = toRad(x2); 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * 
+                Math.sin(dLon/2) * Math.sin(dLon/2);  
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; 
+    return d;
+  }
+}
+
 
 // Function used in navigator.geolocation.getCurrentPosition method.
 function placeGeoLoc(pos) {
   var lat = pos.coords.latitude;
   var lng = pos.coords.longitude;
   var latlng = new google.maps.LatLng(lat,lng);
+  myPos=pos;
 
   myMarker = new google.maps.Marker({
     position: latlng,
     title:"You are here!"    	
   });
-  
+  geoLoaded=true;
+  var distToMe=hdistance(lat,lng);
+  var mindist=-1;
+  var minkey;
+  for(key in rlstations){
+      newdist=distToMe(rlstations[key]["stop_lat"],rlstations[key]["stop_lon"]);
+      if(newdist<mindist||mindist==-1){
+          minkey=key;
+          mindist=newdist;
+      }
+  }
+  var infowindow = new google.maps.InfoWindow({
+    content: "<p>Closest station is "+minkey+" which is "+mindist.toFixed(3)+" miles away</p>"
+  });
+  google.maps.event.addListener(myMarker, 'click', function() { 
+    infowindow.open(map, myMarker);
+  }); 
   myMarker.setMap(map);
 }
 
@@ -73,10 +102,10 @@ function placeRedLineStations(){
   for(key in rlstations){
     var station = rlstations[key];
     var pos = new google.maps.LatLng(station["stop_lat"],station["stop_lon"]);
-    redLineMarker[key]=createStationMarker(pos,key);
-    redLineMarker[key]["marker"].setMap(map);   		
-	}
-	drawStationConnections();
+    redLineMarkers[key]=createStationMarker(pos,key);
+    redLineMarkers[key]["marker"].setMap(map);   		
+  }
+  drawStationConnections();
 }
 
 function createStationMarker(pos, t) {
@@ -131,10 +160,6 @@ function setArrivalData(){
 function infoWindowCallback(){
   if(dRequest.readyState==4&&dRequest.status==200){
     arrivalData=JSON.parse(dRequest.responseText);
-    for(i=0; i<arrivalData.length;i++){
-      //if(arrivalData[i]["PlatformKey"]=="RBRAS")console.log("rbras:"+arrivalData[i]["Route"]);
-      //if(arrivalData[i]["PlatformKey"]=="RASHS")console.log("rashs:"+arrivalData[i]["Route"]);
-    }
     for(key in rlstations){
       setInfo(key);
     } 
@@ -146,10 +171,10 @@ function infoWindowCallback(){
 }
 
 function setInfo(key){
-  var infoWindow=redLineMarker[key]["info"];
+  var infoWindow=redLineMarkers[key]["info"];
   var nbname=rlstations[key]["PlatformKeyNB"];
   var sbname=rlstations[key]["PlatformKeySB"];
-  var content="<h3>"+key+" STATION:</h3><table border=\"1\"><tr><th>Direction</th><th>Predicted Arrival</th></tr>";
+  var content="<p class=\"infoheader\">"+key+" STATION:</p><table border=\"1\"><tr><th>Direction</th><th>Predicted Arrival</th></tr>";
   for(i=0; i<arrivalData.length; i++){
     if(arrivalData[i]["PlatformKey"]==nbname&&arrivalData[i]["InformationType"]=="Predicted"){
       content+="<tr><td>Alewife (NB)</td><td>"+arrivalData[i]["Time"]+"</td></tr>";
@@ -165,13 +190,10 @@ function setInfo(key){
 }
 
 function drawStationConnections(){
-  //assumption, there are three branches, trunk, Braintree, and Ashmont, 
-  //and Braintree and Ashmont split off from the trunk
   for(i=0; i<connections.length; i++){
     var rlcoord = [];
     for(j=0; j<connections[i].length; j++){
       var skey=connections[i][j];
-      //console.log(skey);
       var pos=new google.maps.LatLng(rlstations[skey]["stop_lat"],rlstations[skey]["stop_lon"]);
       rlcoord.push(pos);
     }
@@ -186,7 +208,7 @@ function drawStationConnections(){
 }
 
 
-function getCarmenAndWaldo(){
+function setCarmenAndWaldo(){
   try {
     cwRequest = new XMLHttpRequest();
   }
@@ -215,33 +237,21 @@ function getCarmenAndWaldo(){
 function drawCarmenAndWaldoCallback(){
   if(cwRequest.readyState==4&&cwRequest.status==200){
     var cwdata=JSON.parse(cwRequest.responseText);
-    console.log(cwdata);
     for(i=0; i<cwdata.length;i++){
       var image = 'stop_marker.png';
       if(cwdata[i]["name"]=="Waldo")waldo=true;
       if(cwdata[i]["name"]=="Carmen Sandiego")carmen=true;
       var marker=createCWMarker(cwdata[i]);
       marker.setMap(map);
-      people[cwdata[i]["name"]]=new Object();
-      people[cwdata[i]["name"]]["data"]=cwdata[i];
-      people[cwdata[i]["name"]]["marker"]=marker;
     }
-    if(!waldo && !carmen){
-      alert("Neither Carmen nor Waldo could be found!");
-    }else if(!waldo){
-      alert("Wally couldn't be found!");
-    }else if(!carmen){
-      alert("Carmen couldn't be found!");
-    }	
-	}
+  }
   if(cwRequest.status==0||cwRequest.status==404){	
     cwRequest.abort();
-    alert("Neither Carmen nor Waldo could be found! Oh no! Refresh the page to try again");
   }
 }
 
 function createCWMarker(obj){
-  var image = 'stop_marker.png';
+  var image;
   if(obj["name"]=="Waldo"){
     image = 'waldo.png';
   }else if(obj["name"]=="Carmen Sandiego"){
@@ -253,176 +263,333 @@ function createCWMarker(obj){
     title: obj["name"],
     icon: image
   });
-  var infowindow = new google.maps.InfoWindow({
-    content: obj["loc"]["note"]
-  });
+  var infowindow = new google.maps.InfoWindow({});
+  var note=obj["loc"]["note"];
   google.maps.event.addListener(marker, 'click', function() { 
-    /*infowindow.setContent('test: ' + i + '');*/
+    var content="<p>Note: "+note+"<p>";
+    if(geoLoaded==true){
+      var distToMe = hdistance(myPos.coords.latitude,myPos.coords.longitude);
+      var mi=distToMe(obj["loc"]["latitude"],obj["loc"]["longitude"]); 
+      content+="<p>"+obj["name"]+" is "+ mi.toFixed(3) +" miles away from you!<p>";
+    }
+    infowindow.setContent(content);
     infowindow.open(map, marker);
   }); 
   return marker;
 }
 
-/*var redLineRequest;
-function placeRedLineStations(){
-	try {
-		redLineRequest = new XMLHttpRequest();
-	 }
-	 catch (ms1) { 
-	 	try {
-	 		redLineRequest = new ActiveXObject("Msxml2.XMLHTTP");
-	 	}
-	 	catch (ms2) {
-	 		try {
-	 			redLineRequest = new ActiveXObject("Microsoft.XMLHTTP");
-	 		}
-	 		catch (ex) {
-	 			redLineRequest = null;
-	 		}
-	 	}
-	 }
-	 if (redLineRequest == null) {
-		 alert("Error creating request object --Ajax not supported?");
-   	 }else{
-	     redLineRequest.open("GET","RealTimeHeavyRailKeys.csv",true);    
-	     redLineRequest.send(null);
-	     redLineRequest.onreadystatechange=drawRedLineStations;
-     }
-}*/
-
-/*function drawRedLineStations(){
-	if(redLineRequest.readyState==4&&redLineRequest.status==200){		
-		var allText = redLineRequest.responseText;
-		var allTextLines = [];
-		allTextLines = allText.split(/\r\n|\n/);
-		var fldHeading = [];
-		fldHeading = allTextLines[0].split(',');
-		for(i=0;i<fldHeading.length;i++){
-			//assumption that RealTimeHeavyRailKeys.csv file will always have these as categories
-			if(fldHeading[i]=="StationName")nameOffset=i;
-			if(fldHeading[i]=="stop_lat")latOffset=i;
-			if(fldHeading[i]=="stop_lon")lonOffset=i;
-			if(fldHeading[i]=="PlatformKey")pkeyOffset=i;
-			if(fldHeading[i]=="Line")lineOffset=i;
-			if(fldHeading[i]=="EndOfLine")eolOffset=i;
-			if(fldHeading[i]=="Branch")branchOffset=i;
-			if(fldHeading[i]=="Direction")dirOffset=i;
-			if(fldHeading[i]=="PlatformOrder")orderOffset=i;
-			//is there a find functions to do this better????
-		}
-		
-
-		for(i=1; i<allTextLines.length; i++){
-			keyData[i-1] = allTextLines[i].split(',');
-		}
-		
-
-		var pkeyToStationName = new Object();
-		for(i=0;i<keyData.length;i++){
-			if(keyData[i][lineOffset]=="Red"){
-				stationName=keyData[i][nameOffset];
-				if(keyData[i][eolOffset]=="TRUE"&&keyData[i][dirOffset]=="NB"){
-					stationOrder[0].push(stationName);
-				}else if(keyData[i][dirOffset]=="SB"{
-					stationOrder[keyData[i][orderOffset]].push(stationName);
-				}
-				//console.log("checking for "+stationName);
-				if(redStations[stationName]==null){
-				//	console.log("initializing "+stationName);					
-					redStations[stationName]=new Object();
-					redStations[stationName].lat=keyData[i][latOffset];
-					redStations[stationName].lon=keyData[i][lonOffset];
-					redStations[stationName].pkeys=[keyData[i][pkeyOffset]];
-					redStations[stationName].branch=keyData[i][branchOffset];
-					pkeyToStationName[keyData[i][pkeyOffset]]=stationName;
-				//	console.log("end initializing "+stationName);
-				}else{
-				//	console.log("adding to "+stationName);
-					redStations[stationName].pkeys.push(keyData[i][pkeyOffset]);
-					pkeyToStationName[keyData[i][pkeyOffset]]=stationName;
-				//	console.log("end adding to "+stationName);
-				//	console.log(redStations[stationName]);
-				}
-				
-			}
-		}
-		
-
-		var redLineMarker = new Object();
-		for(key in redStations){
-			var station = redStations[key];
-			var pos = new google.maps.LatLng(station.lat,station.lon);
-			redLineMarker[key]=createStationMarker(pos,key)	
-    		redLineMarker[key].setMap(map);   		
-		}
-		drawStationConnections();
-		
+//hardcoded station data:
+rlstations= 
+{
+"ALEWIFE":
+	{
+		"StationName": "ALEWIFE",
+		"PlatformKeyNB": "RALEN",
+		"PlatformKeySB": "",
+		"PlatformName": "ALEWIFE NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-alfcl",
+		"stop_name": "Alewife Station",
+		"stop_desc": "",
+		"stop_lat": "42.395428",
+		"stop_lon": "-71.142483"
+	},
+"DAVIS":
+	{
+		"StationName": "DAVIS",
+		"PlatformKeyNB": "RDAVN",
+		"PlatformKeySB": "RDAVS",
+		"PlatformName": "DAVIS NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-davis",
+		"stop_name": "Davis Station",
+		"stop_desc": "",
+		"stop_lat": "42.39674",
+		"stop_lon": "-71.121815"
+	},
+"PORTER":
+	{
+		"StationName": "PORTER",
+		"PlatformKeyNB": "RPORN",
+		"PlatformKeySB": "RPORS",
+		"PlatformName": "PORTER NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-portr",
+		"stop_name": "Porter Square Station",
+		"stop_desc": "",
+		"stop_lat": "42.3884",
+		"stop_lon": "-71.119149"
+	},
+"HARVARD":
+	{
+		"StationName": "HARVARD",
+		"PlatformKeyNB": "RHARN",
+		"PlatformKeySB": "RHARS",
+		"PlatformName": "HARVARD NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-harsq",
+		"stop_name": "Harvard Square Station",
+		"stop_desc": "",
+		"stop_lat": "42.373362",
+		"stop_lon": "-71.118956"
+	},
+"CENTRAL":
+	{
+		"StationName": "CENTRAL",
+		"PlatformKeyNB": "RCENN",
+		"PlatformKeySB": "RCENS",
+		"PlatformName": "CENTRAL NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-cntsq",
+		"stop_name": "Central Square Station",
+		"stop_desc": "",
+		"stop_lat": "42.365486",
+		"stop_lon": "-71.103802"
+	},
+"KENDALL":
+	{
+		"StationName": "KENDALL",
+		"PlatformKeyNB": "RKENN",
+		"PlatformKeySB": "RKENS",
+		"PlatformName": "KENDALL NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-knncl",
+		"stop_name": "Kendall/MIT Station",
+		"stop_desc": "",
+		"stop_lat": "42.36249079",
+		"stop_lon": "-71.08617653"
+	},
+"CHARLES MGH":
+	{
+		"StationName": "CHARLES MGH",
+		"PlatformKeyNB": "RMGHN",
+		"PlatformKeySB": "RMGHS",
+		"PlatformName": "CHARLES MGH NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-chmnl",
+		"stop_name": "Charles/MGH Station",
+		"stop_desc": "",
+		"stop_lat": "42.361166",
+		"stop_lon": "-71.070628"
+	},
+"PARK":
+	{
+		"StationName": "PARK",
+		"PlatformKeyNB": "RPRKN",
+		"PlatformKeySB": "RPRKS",
+		"PlatformName": "PARK NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-pktrm",
+		"stop_name": "Park St. Station",
+		"stop_desc": "",
+		"stop_lat": "42.35639457",
+		"stop_lon": "-71.0624242"
+	},
+"DOWNTOWN CROSSING":
+	{
+		"StationName": "DOWNTOWN CROSSING",
+		"PlatformKeyNB": "RDTCN",
+		"PlatformKeySB": "RDTCS",
+		"PlatformName": "DOWNTOWN CROSSING NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-dwnxg",
+		"stop_name": "Downtown Crossing Station",
+		"stop_desc": "",
+		"stop_lat": "42.355518",
+		"stop_lon": "-71.060225"
+	},
+"SOUTH STATION":
+	{
+		"StationName": "SOUTH STATION",
+		"PlatformKeyNB": "RSOUN",
+		"PlatformKeySB": "RSOUS",
+		"PlatformName": "SOUTH STATION NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-sstat",
+		"stop_name": "South Station",
+		"stop_desc": "",
+		"stop_lat": "42.352271",
+		"stop_lon": "-71.055242"
+	},
+"BROADWAY":
+	{
+		"StationName": "BROADWAY",
+		"PlatformKeyNB": "RBRON",
+		"PlatformKeySB": "RBROS",
+		"PlatformName": "BROADWAY NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-brdwy",
+		"stop_name": "Broadway Station",
+		"stop_desc": "",
+		"stop_lat": "42.342622",
+		"stop_lon": "-71.056967"
+	},
+"ANDREW":
+	{
+		"StationName": "ANDREW",
+		"PlatformKeyNB": "RANDN",
+		"PlatformKeySB": "RANDS",
+		"PlatformName": "ANDREW NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-andrw",
+		"stop_name": "Andrew Station",
+		"stop_desc": "",
+		"stop_lat": "42.330154",
+		"stop_lon": "-71.057655"
+	},
+"JFK":
+	{
+		"StationName": "JFK",
+		"PlatformKeyNB": "RJFKN",
+		"PlatformKeySB": "RJFKS",
+		"PlatformName": "JFK NB",
+		"Branch": "Trunk",
+		"Direction": "NB",
+		"stop_id": "place-jfkred",
+		"stop_name": "JFK/UMass Station",
+		"stop_desc": "",
+		"stop_lat": "42.320685",
+		"stop_lon": "-71.052391"
+	},
+"SAVIN HILL":
+	{
+		"StationName": "SAVIN HILL",
+		"PlatformKeyNB": "RSAVN",
+		"PlatformKeySB": "RSAVS",
+		"PlatformName": "SAVIN HILL NB",
+		"Branch": "Ashmont",
+		"Direction": "NB",
+		"stop_id": "place-shmnl",
+		"stop_name": "Savin Hill Station",
+		"stop_desc": "",
+		"stop_lat": "42.31129",
+		"stop_lon": "-71.053331"
+	},
+"FIELDS CORNER":
+	{
+		"StationName": "FIELDS CORNER",
+		"PlatformKeyNB": "RFIEN",
+		"PlatformKeySB": "RFIES",
+		"PlatformName": "FIELDS CORNER NB",
+		"Branch": "Ashmont",
+		"Direction": "NB",
+		"stop_id": "place-fldcr",
+		"stop_name": "Fields Corner Station",
+		"stop_desc": "",
+		"stop_lat": "42.300093",
+		"stop_lon": "-71.061667"
+	},
+"SHAWMUT":
+	{
+		"StationName": "SHAWMUT",
+		"PlatformKeyNB": "RSHAN",
+		"PlatformKeySB": "RSHAS",
+		"PlatformName": "SHAWMUT NB",
+		"Branch": "Ashmont",
+		"Direction": "NB",
+		"stop_id": "place-smmnl",
+		"stop_name": "Shawmut Station",
+		"stop_desc": "",
+		"stop_lat": "42.29312583",
+		"stop_lon": "-71.06573796"
+	},
+"ASHMONT":
+	{
+		"StationName": "ASHMONT",
+		"PlatformKeyNB": "",
+		"PlatformKeySB": "RASHS",
+		"PlatformName": "ASHMONT SB",
+		"Branch": "Ashmont",
+		"Direction": "SB",
+		"stop_id": "place-asmnl",
+		"stop_name": "Ashmont Station",
+		"stop_desc": "",
+		"stop_lat": "42.284652",
+		"stop_lon": "-71.064489"
+	},
+"NORTH QUINCY":
+	{
+		"StationName": "NORTH QUINCY",
+		"PlatformKeyNB": "RNQUN",
+		"PlatformKeySB": "RNQUS",
+		"PlatformName": "NORTH QUINCY NB",
+		"Branch": "Braintree",
+		"Direction": "NB",
+		"stop_id": "place-nqncy",
+		"stop_name": "North Quincy Station",
+		"stop_desc": "",
+		"stop_lat": "42.275275",
+		"stop_lon": "-71.029583"
+	},
+"WOLLASTON":
+	{
+		"StationName": "WOLLASTON",
+		"PlatformKeyNB": "RWOLN",
+		"PlatformKeySB": "RWOLS",
+		"PlatformName": "WOLLASTON NB",
+		"Branch": "Braintree",
+		"Direction": "NB",
+		"stop_id": "place-wlsta",
+		"stop_name": "Wollaston Station",
+		"stop_desc": "",
+		"stop_lat": "42.2665139",
+		"stop_lon": "-71.0203369"
+	},
+"QUINCY CENTER":
+	{
+		"StationName": "QUINCY CENTER",
+		"PlatformKeyNB": "RQUCN",
+		"PlatformKeySB": "RQUCS",
+		"PlatformName": "QUINCY CENTER NB",
+		"Branch": "Braintree",
+		"Direction": "NB",
+		"stop_id": "place-qnctr",
+		"stop_name": "Quincy Center Station",
+		"stop_desc": "",
+		"stop_lat": "42.251809",
+		"stop_lon": "-71.005409"
+	},
+"QUINCY ADAMS":
+	{
+		"StationName": "QUINCY ADAMS",
+		"PlatformKeyNB": "RQUAN",
+		"PlatformKeySB": "RQUAS",
+		"PlatformName": "QUINCY ADAMS NB",
+		"Branch": "Braintree",
+		"Direction": "NB",
+		"stop_id": "place-qamnl",
+		"stop_name": "Quincy Adams Station",
+		"stop_desc": "",
+		"stop_lat": "42.233391",
+		"stop_lon": "-71.007153"
+	},
+"BRAINTREE":
+	{
+		"StationName": "BRAINTREE",
+		"PlatformKeyNB": "RBRAS",
+		"PlatformKeySB": "RBRAS",
+		"PlatformName": "BRAINTREE SB",
+		"Branch": "Braintree",
+		"Direction": "SB",
+		"stop_id": "place-brntn",
+		"stop_name": "Braintree Station",
+		"stop_desc": "",
+		"stop_lat": "42.2078543",
+		"stop_lon": "-71.0011385"
 	}
-	if(redLineRequest.status==0||redLineRequest.status==404){	
-		redLineRequest.abort();
-		placeRedLineStations();
-	}
-}*/
-
-
-/*
-var redLineRequest;
-function load_redLineData() {
-	 try {
-		redLineRequest = new XMLHttpRequest();
-	 }
-	 catch (ms1) { 
-	 	try {
-	 		redLineRequest = new ActiveXObject("Msxml2.XMLHTTP");
-	 	}
-	 	catch (ms2) {
-	 		try {
-	 			redLineRequest = new ActiveXObject("Microsoft.XMLHTTP");
-	 		}
-	 		catch (ex) {
-	 			redLineRequest = null;
-	 		}
-	 	}
-	 }
-  
-	 if (redLineRequest == null) {
-		 alert("Error creating request object --Ajax not supported?");
-   	 }else{
-	     redLineRequest.open("GET","http://mbtamap-cedar.herokuapp.com/mapper/redline.json",true);    
-	     redLineRequest.send(null);
-	     redLineRequest.onreadystatechange=redLineCallback;
-     }
-}
-
-function redLineCallback(){
-	if(redLineRequest.readyState==4&&redLineRequest.status==200){
-		placeRedLine(redLineRequest.responseText);
-	}
-	if(redLineRequest.status==0||redLineRequest.status==404){	
-		redLineRequest.abort();
-		load_redLineData();
-	}
-}
-
-function placeRedLine(str){
-	data=JSON.parse(str);
-	
-	for(i=0; i<parsed.length; i++){
-		
-		 	redlineMarkers[i] = new google.maps.Marker({
-    		position: new google.maps.LatLng(parsed[i].lattitude,parsed[i].longitude),
-    		title:"Hello World!"
-    	});
-    
-   
-		
-	    c.innerHTML=parsed[i]["company"];
-	    c=r.insertCell(1);
-	    c.innerHTML=parsed[i]["location"];
-	    c=r.insertCell(2);
-	    c.innerHTML=parsed[i]["position"];
-	}
-	 yourLoc.setMap(map);
-}
-
-*/
+};
+connections=[["ALEWIFE","DAVIS","PORTER","HARVARD","CENTRAL","KENDALL","CHARLES MGH","PARK","DOWNTOWN CROSSING","SOUTH STATION","BROADWAY","ANDREW","JFK"],
+["JFK","SAVIN HILL","FIELDS CORNER","SHAWMUT","ASHMONT"],
+["JFK","NORTH QUINCY","WOLLASTON","QUINCY CENTER","QUINCY ADAMS","BRAINTREE"]];
